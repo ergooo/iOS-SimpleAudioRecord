73ff19cf37b5b4599fc2ac940d5b64584c71f404
masato <goodmoon.badmoon@gmail.com>
2020-05-05 23:27:48 +0900

    first commit

---
+++ SimpleAudioRecord/SimpleAudioRecord.h
@@ -0,0 +1,19 @@
+//
+//  SimpleAudioRecord.h
+//  SimpleAudioRecord
+//
+//  Created by masato on 2020/05/05.
+//  Copyright © 2020 masato. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleAudioRecord.
+FOUNDATION_EXPORT double SimpleAudioRecordVersionNumber;
+
+//! Project version string for SimpleAudioRecord.
+FOUNDATION_EXPORT const unsigned char SimpleAudioRecordVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleAudioRecord/PublicHeader.h>
+
+
---
+++ SimpleAudioRecordTests/SimpleAudioRecordTests.swift
@@ -0,0 +1,34 @@
+//
+//  SimpleAudioRecordTests.swift
+//  SimpleAudioRecordTests
+//
+//  Created by masato on 2020/05/05.
+//  Copyright © 2020 masato. All rights reserved.
+//
+
+import XCTest
+@testable import SimpleAudioRecord
+
+class SimpleAudioRecordTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
