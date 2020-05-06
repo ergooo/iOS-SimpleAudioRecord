a6fd00de8564db5e644f8a59629a8efcc131279e
masato <goodmoon.badmoon@gmail.com>
2020-05-06 15:34:43 +0900

    added new Example project

---
+++ Example/Source/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Example
+//  
+//  Created by masato on 2020/05/06
+//  Copyright © 2020 masato. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
